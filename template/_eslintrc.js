787a6b494beb1932cda4b4fa588091ce0397f91c
delyethan <h.delyethan123@gmail.com>
2019-10-28 14:00:25 +0700

    update eslint

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  parser: 'babel-eslint',
+  parserOptions: {
+      ecmaVersion: 8,
+      sourceType: 'module',
+      ecmaFeatures: {
+          jsx: true,
+      },
+  },
+}
