c9ae243ecee987b6201f678c46e85b8af1d8a2f8
delyethan <h.delyethan123@gmail.com>
2022-11-23 17:22:04 +0700

    upgrade 0.70.x

--- template/app/screens/Home/HomeScreen.tsx
+++ template/app/screens/Home/HomeScreen.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text } from 'react-native'
 import _styles from '@styles'
 import { styles } from './HomeStyle'
+import SafeArea from 'components/SafeArea'
 
-export interface Props { }
+export interface Props {}
 
 export default function HomeScreen(props: Props): React.ReactElement {
-
   return (
     <View style={_styles.flx1}>
-      <SafeAreaView>
+      <SafeArea edges={'safeTop'}>
         <Text>Home Screen</Text>
-      </SafeAreaView>
+      </SafeArea>
     </View>
   )
 }
-
